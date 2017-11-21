72e79fe74e785c91f2bf4062ed65f12f0ded4bcb
Sam K <kolass11@uwosh.edu>
2017-11-20 21:18:02 -0600

    Added Jabriel's update_question_status to the queries file

--- Main/Database_files/queries.php
+++ Main/Database_files/queries.php
@@ -192,4 +192,26 @@
     }
   }
 
+
+  function update_question_status($questionId) {
+    
+    global $db;
+  
+    try {
+      $query = "UPDATE `questions` 
+                SET `status` = inactive
+                WHERE `question_id` = '$questionId'";
+    
+      $stmt = $db->prepare($query);
+      $stmt->execute([]);
+      return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    } 
+    catch (PDOException $e) {
+        db_disconnect();
+        exit("Aborting: There was a database error when updating the status of the question");
+    }
+  
+    }
+
 ?>
