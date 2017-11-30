ec6953b47ff2d48a4da8d45c1d7226e4e3781fee
Michael Drum <drumm91@uwosh.edu>
2017-11-29 22:31:11 -0600

    Made it so the instructor can activate one question at a time.

---
+++ Main/Database_files/attempttoactivatequestion.php
@@ -0,0 +1,18 @@
+<?php
+require_once('initialize.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    $id = $_POST['questionId'];
+	if(attempt_to_activate_question($id)){
+		echo "Question " . $id . " was activated.";
+	}else{
+		echo "A question was not activated.";
+	}
+}
+function attempt_to_activate_question($id){
+	//deactivate all active questions
+	deactivate_all_activated_questions();
+	//activate question
+	return set_status_activated($id);
+}
+?>
